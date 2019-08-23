e6bfd8825cb7459e6f0a143b642cc4313fe9a417
Mathew Williamson <mwilliamson@64labs.com>
2019-08-22 22:43:41 -0400

    Added to the UserGrid

---
+++ src/components/SingleUser.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+interface SingleUserProps {
+    singleUser: any
+}
+
+const SingleUser = ({singleUser}: SingleUserProps) => {
+    const {location} = singleUser
+
+    return (
+        <div className="User_wrapper">
+            <div className="User_container">
+                <h3 className="User_name">{singleUser.name.first} {singleUser.name.last}</h3>
+                <img src={singleUser.picture.large}/>
+                <div className="User_location">
+                    <p>{location.street}</p>
+                    <p>{location.city}, {location.state} {location.postcode}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SingleUser
---
+++ src/components/UserGrid.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react'
+import SingleUser from './SingleUser'
+
+// interface UserGridState {
+//     data: any, // TODO Cheated here, fix all the any types!!!
+//     isLoaded: Boolean,
+//     filterValue: String,
+// }
+
+const UserGrid: React.FC = () => {
+    const [data, setData] = useState<any>({ data: [] })
+    const [isLoaded, setIsLoaded] = useState({isLoaded: false})
+    const [filterValue, setFilterValue] = useState('')
+    // const results = data.results
+    console.log('[matt] filterValue', filterValue)
+    console.log('[matt] data', data)
+    
+
+    useEffect(() => {
+        fetch('https://randomuser.me/api/?inc=name,location,picture&results=10')
+            .then(res => res.json())
+            .then(resData => {
+                console.log('[matt][tealium] resData', resData)
+                
+                setData(resData)
+                setIsLoaded({isLoaded: true})
+            })
+    }, []);
+
+    if (!isLoaded) {
+        return <div>...Loading</div>
+    }
+
+    return (
+        <div>
+            <input
+                type="text"
+                value={String(filterValue)}
+                onChange={(e) => setFilterValue(String(e.target.value))}
+                placeholder={"Type here to filter by name"}
+            />
+            <div className="UserGrid_container">
+                {data && data.results && data.results.length > 0 && data.results
+                    .filter((person:any, idx:number) => {
+                        const fullName = `${person.name.first} ${person.name.last}`
+                        // return fullName.includes(this.state.filterValue.toLowerCase()) && idx < 9
+                    })
+                    .map((person:any, idx:number) => <SingleUser singleUser={person} key={idx} />)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default UserGrid
